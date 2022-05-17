e65adf7b0f8188da1c262eb02e5f15a28def9974
Jueun Grace Yun <graceyunn@gmail.com>
2022-05-16 23:55:54 -0400

    clean up

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
+
+    return config
+  },
+  swcMinify: true,
+  // experimental: {
+  //   lodash: {
+  //     transform: 'lodash/{{member}}',
+  //   },
+  // },
 }
 
 module.exports = nextConfig
