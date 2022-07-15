927fd3107b22e691cd730e3f9d640e7f02abd2e0
Jueun Grace Yun <graceyunn@gmail.com>
2022-07-15 13:30:47 -0400

    env and shop status

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,7 +1,28 @@
 import { ShopLayout } from '@components/shop'
+import { useEffect, useState } from 'react'
 
-const Shop = () => <div className={'h-[200vh]'}>hey store</div>
+const Shop = () => {
+  const [shopStatus, setShopStatus] = useState('default')
 
-Shop.getLayout = (page: JSX.Element | JSX.Element[]) => <ShopLayout>{page}</ShopLayout>
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_SHOP_STATUS) setShopStatus(process.env.NEXT_PUBLIC_SHOP_STATUS)
+  }, [])
+
+  if (typeof window !== 'undefined') {
+    switch (shopStatus) {
+      case 'live':
+        return <div className={'h-[200vh]'}>hey store</div>
+      case 'comingsoon':
+        return <p>coming soon</p>
+      case 'maintenance':
+        return <p>maintenance</p>
+      default:
+        return null
+    }
+  }
+}
+
+if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_SHOP_STATUS === 'live')
+  Shop.getLayout = (page: JSX.Element | JSX.Element[]) => <ShopLayout>{page}</ShopLayout>
 
 export default Shop
