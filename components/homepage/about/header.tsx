52e724d69966901b4cad8c4c2171e8296a16fde1
Jueun Grace Yun <graceyunn@gmail.com>
2023-01-22 18:37:15 -0500

    About me section

--- components/homepage/about/header.tsx
+++ components/homepage/about/header.tsx
@@ -11,8 +11,9 @@ export function Header() {
       <motion.h1
         initial={{ y: 20, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
-        transition={{ delay: 0.25, duration: 1, ease: 'backInOut' }}
+        transition={{ duration: 1, ease: 'backInOut' }}
         className={libreCaslonText.className}
+        viewport={{ once: true }}
       >
         <ItalicText text='About&nbsp;Me' italics={[1, 6]} />
       </motion.h1>
--- components/homepage/about/text-and-illustrations.tsx
+++ components/homepage/about/text-and-illustrations.tsx
@@ -1,5 +1,9 @@
+'use client';
+
+import { motion } from 'framer-motion';
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
+import useMediaQuery from '../../../utils/use-media-query';
 import styles from './index.module.css';
 
 const MARKDOWN = [
@@ -7,6 +11,7 @@ const MARKDOWN = [
   'Portrait **photography** hobbyist. Sporadic **doodler**. Cozy Switch game enthusiast. Currently waiting patiently for the release of Octopath Traveler 2.',
   `**${new Date().getFullYear()}**`,
 ];
+
 const ILLUSTRATIONS = [
   { src: '/vercel_growth_team.svg', alt: '' },
   { src: '/nzxt_ecommerce.svg', alt: '' },
@@ -14,22 +19,77 @@ const ILLUSTRATIONS = [
 ];
 
 export function TextAndIllustrations() {
+  const isSmallerViewport = useMediaQuery('(max-width: 52rem)');
+
+  const container = {
+    hide: { opacity: 0 },
+    enter: {
+      opacity: 1,
+      transition: { staggerChildren: 0.25, delayChildren: 0.5 },
+    },
+  };
+
+  const markdown = {
+    hide: { x: -30, opacity: 0 },
+    enter: {
+      x: 0,
+      opacity: 1,
+      transition: { duration: 0.5, ease: 'easeInOut' },
+    },
+  };
+
+  const svg = {
+    hide: { opacity: 0 },
+    enter: {
+      opacity: 1,
+      transition: {
+        duration: 0.7,
+        ease: 'easeInOut',
+      },
+    },
+  };
+
+  const containerAttributes = isSmallerViewport
+    ? { initial: { opacity: 1 } }
+    : {
+        variants: container,
+        initial: 'hide',
+        whileInView: 'enter',
+      };
+
+  const childAttributes = isSmallerViewport
+    ? {
+        initial: 'hide',
+        whileInView: 'enter',
+        transition: { duration: 1 },
+        viewport: { once: true },
+      }
+    : null;
+
   return (
-    <div className={styles.grid}>
+    <motion.div {...containerAttributes} className={styles.grid}>
       {MARKDOWN.map((text, i) => (
-        <ReactMarkdown key={`${text}-${i}`}>{text}</ReactMarkdown>
+        <motion.div
+          {...childAttributes}
+          key={`${text}-${i}`}
+          variants={markdown}
+        >
+          <ReactMarkdown>{text}</ReactMarkdown>
+        </motion.div>
       ))}
       {ILLUSTRATIONS.map(({ src, alt }, i) => (
-        <div
+        <motion.div
+          {...childAttributes}
           key={`${src}-${i}`}
           className={styles['image-container']}
           style={{ gridArea: `image-${i + 1}` }}
+          variants={svg}
         >
           <div className={styles['inner-image-container']}>
             <Image src={src} alt={alt} fill />
           </div>
-        </div>
+        </motion.div>
       ))}
-    </div>
+    </motion.div>
   );
 }
