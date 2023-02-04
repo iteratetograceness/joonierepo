3a8fc48f14c25c4b45137b4d87fe5c65dbed8e2d
Jueun Grace Yun <graceyunn@gmail.com>
2023-02-03 19:51:34 -0500

    readd svg

--- app/head.tsx
+++ app/head.tsx
@@ -11,7 +11,7 @@ export default function Head() {
         property='og:image'
         content='https://og-joonie.vercel.app/api/og?token=bd8be8ce84a46a56a74e57df7f3c59ec042b4e9e730a62040a8ef1a55612aebd&title=JUEUNGRACEYUN&description=Software%20Engineer%20and%20Illustrator&hideName=true&italics=1,6,11'
       />
-
+      <link rel='icon' href='/blob.svg' />
       <meta property='og:url' content='https://joonie.dev' />
       <meta property='og:type' content='website' />
       <meta property='og:title' content='Jueun Grace Yun' />
