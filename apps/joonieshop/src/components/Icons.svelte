7e7e685e036251ccf5528508d7bf6927c0dae7c3
Jueun Grace Yun <graceyunn@gmail.com>
2023-02-20 16:32:54 -0500

    joonieshop

--- apps/joonieshop/src/components/Icons.svelte
+++ apps/joonieshop/src/components/Icons.svelte
@@ -93,42 +93,5 @@
         d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"
       />
     </svg>
-  {:else if type === 'arrowLeft'}
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={strokeColor}
-      ><path d="M19 12H5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /><path
-        d="M12 19L5 12L12 5"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      /></svg
-    >
-  {:else if type === 'arrowRight'}
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke={strokeColor}
-      xmlns="http://www.w3.org/2000/svg"
-      ><path d="M5 12H19" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /><path
-        d="M12 5L19 12L12 19"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      /></svg
-    >
-  {:else if type === 'caretRight'}
-    <svg
-      viewBox="0 0 24 24"
-      width="24"
-      height="24"
-      stroke={strokeColor}
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      fill="none"
-      shape-rendering="geometricPrecision"
-      class="Collapse_icon__JsuEg"><path d="M9 18l6-6-6-6" /></svg
-    >
   {/if}
 </div>
