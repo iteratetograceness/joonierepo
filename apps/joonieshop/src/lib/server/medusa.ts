227f0ef35f8e5da9d81fe73cb9b6f96cf0b449ff
Jueun Grace Yun <graceyunn@gmail.com>
2023-05-05 23:03:35 -0400

    env vars

--- apps/joonieshop/src/lib/server/medusa.ts
+++ apps/joonieshop/src/lib/server/medusa.ts
@@ -1,10 +1,9 @@
 import { MedusaClient } from 'sveltekit-medusa-client';
 import { dev } from '$app/environment';
-import { VITE_DEV_MEDUSA_BACKEND_URL, VITE_MEDUSA_BACKEND_URL } from '$env/static/private';
 
 const BACKEND_URL = dev
-	? import.meta.env.VITE_DEV_MEDUSA_BACKEND_URL || VITE_DEV_MEDUSA_BACKEND_URL
-	: import.meta.env.VITE_MEDUSA_BACKEND_URL || VITE_MEDUSA_BACKEND_URL;
+	? import.meta.env.VITE_DEV_MEDUSA_BACKEND_URL
+	: import.meta.env.VITE_MEDUSA_BACKEND_URL;
 
 console.log('BACKEND_URL', BACKEND_URL);
 export default new MedusaClient(BACKEND_URL);
--- apps/joonieshop/src/utils/stripe/client.ts
+++ apps/joonieshop/src/utils/stripe/client.ts
@@ -1,12 +1,9 @@
 import { loadStripe } from '@stripe/stripe-js';
 import { dev } from '$app/environment';
-import { VITE_STRIPE_KEY, VITE_DEV_STRIPE_KEY } from '$env/static/private';
 
 let stripePromise;
 
-const STRIPE_KEY = dev
-	? import.meta.env.VITE_DEV_STRIPE_KEY || VITE_DEV_STRIPE_KEY
-	: import.meta.env.VITE_STRIPE_KEY || VITE_STRIPE_KEY;
+const STRIPE_KEY = dev ? import.meta.env.VITE_DEV_STRIPE_KEY : import.meta.env.VITE_STRIPE_KEY;
 
 const getStripe = () => {
 	stripePromise = loadStripe(STRIPE_KEY || '');
