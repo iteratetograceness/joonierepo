7e1570a4dfee776bb0ce1125058591ae7d6f2ffd
Jueun Grace Yun <graceyunn@gmail.com>
2023-05-05 23:23:47 -0400

    next config

--- apps/jooniesite/next.config.js
+++ apps/jooniesite/next.config.js
@@ -1,17 +1,17 @@
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.ctfassets.net',
-        port: '',
-        pathname: `/${process.env.CONTENTFUL_SPACE_ID}/**`,
-      },
-    ],
-  },
+	experimental: {
+		appDir: true
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'images.ctfassets.net',
+				port: '3331',
+				pathname: `/${process.env.CONTENTFUL_SPACE_ID}/**`
+			}
+		]
+	}
 };
 
 module.exports = nextConfig;
