763e2af4ce7ccc6edf10d245931767afc6f97ee9
Jueun Grace Yun <graceyunn@gmail.com>
2023-05-31 23:15:58 -0400

    pls

--- apps/joonieshop/src/lib/server/medusa.ts
+++ apps/joonieshop/src/lib/server/medusa.ts
@@ -1,8 +1,7 @@
 import { MedusaClient } from 'sveltekit-medusa-client';
 import { dev } from '$app/environment';
+import { PUBLIC_DEV_MEDUSA_BACKEND_URL, PUBLIC_MEDUSA_BACKEND_URL } from '$env/static/public';
 
-const BACKEND_URL = dev
-	? import.meta.env.VITE_DEV_MEDUSA_BACKEND_URL
-	: import.meta.env.VITE_MEDUSA_BACKEND_URL;
+const BACKEND_URL = dev ? PUBLIC_DEV_MEDUSA_BACKEND_URL : PUBLIC_MEDUSA_BACKEND_URL;
 
 export default new MedusaClient(BACKEND_URL);
