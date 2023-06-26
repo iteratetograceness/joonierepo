b08b8b2d2041eca996f9ecbba6e74d8755a22a53
Jueun Grace Yun <graceyunn@gmail.com>
2023-06-25 21:59:14 -0400

    bro pls

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,15 +1,5 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'turbo',
-    'prettier',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
+  extends: ['turbo', 'prettier'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
 }
