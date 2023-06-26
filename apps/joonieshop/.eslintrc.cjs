51458a7576884fd9a6a5929f518c0655b47f5919
Jueun Grace Yun <graceyunn@gmail.com>
2023-06-25 22:15:43 -0400

    svelte store eslint

--- apps/joonieshop/.eslintrc.cjs
+++ apps/joonieshop/.eslintrc.cjs
@@ -1,14 +1,20 @@
 module.exports = {
 	root: true,
 	extends: ['custom'],
-	plugins: ['svelte3'],
-	ignorePatterns: ['*.cjs'],
+	plugins: ['@typescript-eslint'],
+	ignorePatterns: ['node_modules'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
-		'svelte3/typescript': () => require('typescript')
+		'svelte3/typescript': require('typescript'),
+		'svelte3/ignore-styles': () => true
 	},
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte']
+	},
+	env: {
+		es6: true,
+		browser: true
 	}
 };
